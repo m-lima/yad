b672e9f80278a8d523efba3c5074826b90528007
Marcelo Lima <m-lima@users.noreply.github.com>
2022-11-15 20:39:51 +0100

    Control the forking tests

--- tests/daemonize.rs
+++ tests/daemonize.rs
@@ -1,44 +1,126 @@
-#![cfg(test)]
+use yad::{DaemonError, Error, ErrorCode, Stdio};
 
-fn run_test(test: &'static str) {
-    let child = std::process::Command::new(env!("CARGO_BIN_EXE_test_daemonize"))
+fn can_daemonize() {
+    yad::daemonize().unwrap();
+}
+
+fn can_daemonize_with_init() {
+    yad::daemonize_with_init(|| Ok(())).unwrap();
+}
+
+fn can_daemonize_with_options() {
+    yad::with_options()
+        .stdin(Stdio::Null)
+        .stdout(Stdio::Null)
+        .stderr(Stdio::Null)
+        .daemonize()
+        .unwrap();
+}
+
+fn can_daemonize_with_options_and_init() {
+    yad::with_options()
+        .stdin(Stdio::Null)
+        .stdout(Stdio::Null)
+        .stderr(Stdio::Null)
+        .daemonize_with_init(|| Ok(()))
+        .unwrap();
+}
+
+fn error_after_fork() {
+    let err = yad::with_options()
+        .stdin(Stdio::input("/rooty/mc/rootison"))
+        .daemonize()
+        .unwrap_err();
+
+    assert_eq!(
+        Error::Daemon {
+            error: DaemonError::RedirectStdin,
+            cause: nix::Error::ENOENT,
+        },
+        err
+    );
+}
+
+fn error_during_init() {
+    enum TestError {
+        One = 1,
+        Two = 2,
+    }
+
+    impl From<TestError> for i32 {
+        fn from(e: TestError) -> Self {
+            match e {
+                TestError::One => 1,
+                TestError::Two => 2,
+            }
+        }
+    }
+
+    let error =
+        yad::daemonize_with_init(|| Result::<(), _>::Err(TestError::One.into())).unwrap_err();
+    assert_eq!(Error::Initialization { code: ErrorCode(1) }, error);
+
+    let error =
+        yad::daemonize_with_init(|| Result::<(), _>::Err(TestError::Two.into())).unwrap_err();
+    assert_eq!(Error::Initialization { code: ErrorCode(2) }, error);
+}
+
+fn run_test(exe: &str, test: &'static str) -> bool {
+    let child = std::process::Command::new(exe)
         .arg(test)
         .spawn()
         .unwrap()
         .wait_with_output()
         .unwrap();
 
-    if !child.status.success() {
-        panic!();
+    if child.status.success() {
+        println!("execution daemonize::{test} ... [32mok[m");
+        true
+    } else {
+        println!("execution daemonize::{test} ... [31mfail[m");
+        false
     }
 }
 
-#[test]
-fn can_daemonize() {
-    run_test("can_daemonize");
-}
+fn main() -> std::process::ExitCode {
+    let (exe, target) = {
+        let mut args = std::env::args().filter(|arg| arg != "--nocapture");
+        let exe = args.next().unwrap();
+        let target = args.next();
+        (exe, target)
+    };
 
-#[test]
-fn can_daemonize_with_init() {
-    run_test("can_daemonize_with_init");
-}
+    if let Some(target) = target {
+        match target.as_str() {
+            "can_daemonize" => can_daemonize(),
+            "can_daemonize_with_init" => can_daemonize_with_init(),
+            "can_daemonize_with_options" => can_daemonize_with_options(),
+            "can_daemonize_with_options_and_init" => can_daemonize_with_options_and_init(),
+            "error_after_fork" => error_after_fork(),
+            "error_during_init" => error_during_init(),
+            s => panic!("Invalid test: {s}"),
+        }
 
-#[test]
-fn can_daemonize_with_options() {
-    run_test("can_daemonize_with_options");
-}
+        std::process::ExitCode::SUCCESS
+    } else {
+        println!();
+        let success = [
+            "can_daemonize",
+            "can_daemonize_with_init",
+            "can_daemonize_with_options",
+            "can_daemonize_with_options_and_init",
+            "error_after_fork",
+            "error_during_init",
+        ]
+        .map(|test| run_test(exe.as_str(), test))
+        .into_iter()
+        .all(std::convert::identity);
+        println!();
 
-#[test]
-fn can_daemonize_with_options_and_init() {
-    run_test("can_daemonize_with_options_and_init");
-}
-
-#[test]
-fn error_after_fork() {
-    run_test("error_after_fork");
-}
-
-#[test]
-fn error_during_init() {
-    run_test("error_during_init");
+        if success {
+            std::process::ExitCode::SUCCESS
+        } else {
+            std::process::ExitCode::FAILURE
+        }
+    }
 }
